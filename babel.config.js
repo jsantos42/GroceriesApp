a2c12f638ac7a10e43816126ab9847c410f4dcda
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-02-20 12:34:42 +0100

    fix: configure absolute paths for imports

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,21 @@
 module.exports = {
 	presets: ['module:@react-native/babel-preset'],
-	plugins: ['nativewind/babel', 'module:react-native-dotenv'],
+	plugins: [
+		'nativewind/babel',
+		'module:react-native-dotenv',
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				alias: {
+					'@assets': './src/assets',
+					'@components': './src/components',
+					'@navigation': './src/navigation',
+					'@screens': './src/screens',
+					'@services': './src/services',
+					'@utils': './src/utils',
+				},
+			},
+		],
+	],
 };
