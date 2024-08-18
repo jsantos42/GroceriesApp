17fef56a01fbb4f80897ab9864edb685baccaef5
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-08-18 17:34:28 +0200

    fix: fix onPress() not working on overlapping components on Android
    Overlapping components with absolute position does not seem to allow
    onPress() on Android.

--- src/components/inputField/SuggestionsOverlay.tsx
+++ src/components/inputField/SuggestionsOverlay.tsx
@@ -1,4 +1,4 @@
-import {Platform, Pressable, View} from 'react-native';
+import {Pressable, View} from 'react-native';
 import {Suggestion} from './Suggestion';
 import {getBackgroundColor, useStyles} from '@utils/styles';
 import {useAppContext} from '../AppProvider';
@@ -15,13 +15,11 @@ export const SuggestionsOverlay = ({
 }) => {
 	const {theme} = useAppContext();
 	const styles = useStyles();
-	const suggestionsTopProp = Platform.OS === 'ios' ? 'top-20' : 'top-14';
-	const backdropTopProp = Platform.OS === 'ios' ? 'top-5' : 'top-0';
 
 	return (
 		<>
 			<View
-				className={`absolute ${suggestionsTopProp} z-20 w-full ${getBackgroundColor(theme)}`}
+				className={`z-20 w-full ${getBackgroundColor(theme)}`}
 				style={styles.elevation2}>
 				{suggestions.map(({name, label}) => (
 					<Suggestion
@@ -32,7 +30,7 @@ export const SuggestionsOverlay = ({
 				))}
 			</View>
 			<Pressable
-				className={`absolute ${backdropTopProp} z-10 w-full h-full ${getBackgroundColor(theme)} opacity-50`}
+				className={`z-10 w-full h-full ${getBackgroundColor(theme)} opacity-50`}
 				style={styles.elevation1}
 				onPress={hideSuggestionOverlay}
 			/>
